8392d107f9598fb4a12821190afccbd38d8c45ee
Ailton Menezes <ailtonamenezes@outlook.com>
2023-03-21 08:03:48 -0300

    Adicionado o arquivo src/main.java

---
+++ src/main.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class main {
+    public static void main(String[] args) {
+        telaLogin tLogin = new telaLogin();
+        tLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        tLogin.setVisible(true);
+    }
+}
