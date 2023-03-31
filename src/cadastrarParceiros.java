b8ac9332aeb88feb4180935621c7bee92d60c5b5
Ailton Menezes <ailtonamenezes@outlook.com>
2023-03-30 22:44:11 -0300

    Finalizado o layout da tela src/cadastrarParceiros.java

--- src/cadastrarParceiros.java
+++ src/cadastrarParceiros.java
@@ -1,4 +1,6 @@
 import java.awt.Font;
+
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -41,6 +43,22 @@ public class cadastrarParceiros extends JFrame {
         JLabel labelCEP = new JLabel("CEP:");
         JTextField textCEP = new JTextField();
         JLabel labelContato = new JLabel("Contato:");
+        JLabel labelTelefone1 = new JLabel("Telefone 1:");
+        JTextField textTelefone1 = new JTextField();
+        JLabel labelTelefone2 = new JLabel("Telefone 2:");
+        JTextField textTelefone2 = new JTextField();
+        JLabel labelEmail = new JLabel("Email:");
+        JTextField textEmail = new JTextField();
+        JLabel labelSite = new JLabel("Site:");
+        JTextField textSite = new JTextField();
+        JLabel labelEspecialista = new JLabel("Especialista:");
+        JLabel labelNomeEspecialista = new JLabel("Nome Especialista:");
+        JTextField textNomeEspecialista = new JTextField();
+        JLabel labelEspecialidade = new JLabel("Especialidade:");
+        JTextField textEspecialidade = new JTextField();
+        JButton btnSalvar = new JButton("Salvar");
+        JButton btnLimpar = new JButton("Limpar");
+        JButton btnSair = new JButton("Sair");
 
         // Escolhe o tamanho da font
         titulo.setFont(font);
@@ -69,6 +87,22 @@ public class cadastrarParceiros extends JFrame {
         add(labelCEP);
         add(textCEP);
         add(labelContato);
+        add(labelTelefone1);
+        add(labelTelefone2);
+        add(textTelefone1);
+        add(textTelefone2);
+        add(labelEmail);
+        add(textEmail);
+        add(labelSite);
+        add(textSite);
+        add(labelEspecialista);
+        add(labelNomeEspecialista);
+        add(textNomeEspecialista);
+        add(labelEspecialidade);
+        add(textEspecialidade);
+        add(btnSalvar);
+        add(btnLimpar);
+        add(btnSair);
 
         // Define a posição e o tamanho dos itens
         titulo.setBounds(500, 10, 350, 60);
@@ -94,6 +128,22 @@ public class cadastrarParceiros extends JFrame {
         labelCEP.setBounds(50, 240, 80, 20);
         textCEP.setBounds(80, 240, 80, 20);
         labelContato.setBounds(50, 300, 100, 20);
+        labelTelefone1.setBounds(50, 340, 80, 20);
+        textTelefone1.setBounds(115, 340, 110, 20);
+        labelTelefone2.setBounds(240, 340, 110, 20);
+        textTelefone2.setBounds(310, 340, 110, 20);
+        labelEmail.setBounds(50, 380, 100, 20);
+        textEmail.setBounds(90, 380, 350, 20);
+        labelSite.setBounds(445, 380, 80, 20);
+        textSite.setBounds(474, 380, 300, 20);
+        labelEspecialista.setBounds(50, 420, 100, 20);
+        labelNomeEspecialista.setBounds(50, 450, 350, 20);
+        textNomeEspecialista.setBounds(165, 450, 350, 20);
+        labelEspecialidade.setBounds(530, 450, 350, 20);
+        textEspecialidade.setBounds(620, 450, 350, 20);
+        btnSalvar.setBounds(400, 600, 100, 20);
+        btnLimpar.setBounds(600, 600, 100, 20);
+        btnSair.setBounds(800, 600, 100, 20);
     }
 
     public static void main(String[] args) {
