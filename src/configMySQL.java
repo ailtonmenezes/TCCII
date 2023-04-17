a85d01bcb42fb479a02bfabe0d2eab2ed4314bb1
Ailton Menezes <ailtonamenezes@outlook.com>
2023-04-16 22:44:30 -0300

    Adicionado o arquivo src/configMySQL.java

---
+++ src/configMySQL.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class configMySQL {
+    private static configMySQL instance = null;
+    private Connection connection = null;
+
+    private configMySQL() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/unicesumartcc",
+                    "root",
+                    "KMvd96ui45!");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static configMySQL getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new configMySQL();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new configMySQL();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
